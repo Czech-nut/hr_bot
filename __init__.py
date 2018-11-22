69b514e5e1642e1af4e6d154a434a3cf5de24731
Valery <valeria.shcherba@mifort.org>
2018-11-22 22:22:38 +0300

    'msg' to string and brought concatination back

--- __init__.py
+++ __init__.py
@@ -22,17 +22,16 @@ def read_email_from_gmail():
         first_email_id = int(id_list[0])
         latest_email_id = int(id_list[-1])
 
-
         for i in range(latest_email_id,first_email_id, -1):
             typ, data = mail.fetch(str(i).encode(), '(RFC822)')
 
             for response_part in data:
                 if isinstance(response_part, tuple):
                     msg = email.message_from_string(str(response_part[1]))
-                    email_subject = msg['subject']
-                    email_from = msg['from']
-                    print('From : ', email_from, '\n')
-                    print('Subject : ', email_subject, '\n')
+                    email_subject = str(msg['subject'])
+                    email_from = str(msg['from'])
+                    print('From : ' + email_from + '\n')
+                    print('Subject : ' + email_subject + '\n')
 
     except Exception as e:
         print(str(e))
