3eaae2dd4d14c8c39dd00dc8f4dc5efba5f0d8b2
Oleg <otto.brudershaft@gmail.com>
2018-12-11 12:46:49 +0300

    added pyzmail

--- __init__.py
+++ __init__.py
@@ -1,39 +1,15 @@
-import smtplib
-import time
-import imaplib
-import email
+import imapclient
+import pyzmail
 
-ORG_EMAIL = "@gmail.com"
-FROM_EMAIL = "valery.voitova" + ORG_EMAIL
-FROM_PWD = "hrbottest2206"
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT = 993
+def get_emails_body():
+    imapObj = imapclient.IMAPClient('imap.gmail.com', ssl = True)
+    imapObj.login('valery.voitova@gmail.com', 'hrbottest2206')
+    imapObj.select_folder('Candidates', readonly = True)
+    UIDs = imapObj.search(['UNANSWERED'])
+    rawMessages = imapObj.fetch(UIDs, ['BODY[]'])
 
-def read_email_from_gmail():
-    try:
-        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-        mail.login(FROM_EMAIL,FROM_PWD)
-        mail.select('inbox')
+    def parse_raw_emails():
+        message = pyzmail.PyzMessage.factory(rawMessages['BODY'])
+        print(message)
 
-        type, data = mail.search(None, 'ALL')
-        mail_ids = data[0]
-
-        id_list = mail_ids.split()
-        first_email_id = int(id_list[0])
-        latest_email_id = int(id_list[-1])
-
-        for i in range(latest_email_id,first_email_id, -1):
-            typ, data = mail.fetch(str(i).encode(), '(RFC822)')
-
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_string(str(response_part[1]))
-                    email_subject = str(msg['subject'])
-                    email_from = str(msg['from'])
-                    print('From : ' + email_from + '\n')
-                    print('Subject : ' + email_subject + '\n')
-
-    except Exception as e:
-        print(str(e))
-
-read_email_from_gmail()
+get_emails_body()
