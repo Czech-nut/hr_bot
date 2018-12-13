1d1527745109ce2e1e5fab4d97a565f3886c9ef9
Oleg <otto.brudershaft@gmail.com>
2018-12-13 21:36:28 +0300

    'msg' to string and brought concatination back

--- __init__.py
+++ __init__.py
@@ -4,12 +4,12 @@ import pyzmail
 def get_emails_body():
     imapObj = imapclient.IMAPClient('imap.gmail.com', ssl = True)
     imapObj.login('valery.voitova@gmail.com', 'hrbottest2206')
-    imapObj.select_folder('Candidates', readonly = True)
+    imapObj.select_folder('Candidates', readonly = False)
     UIDs = imapObj.search(['UNANSWERED'])
     rawMessages = imapObj.fetch(UIDs, ['BODY[]'])
 
-    def parse_raw_emails():
-        message = pyzmail.PyzMessage.factory(rawMessages['BODY'])
-        print(message)
+    message = pyzmail.PyzMessage.factory(rawMessages['BODY'])
+    message_body = message.text_part.get_payload().decode(message.text_part.charset)
+    print(message_body)
 
 get_emails_body()
