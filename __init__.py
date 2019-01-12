2d10025cd92bd972fad279a785d0d96ed8b51f27
Oleg <otto.brudershaft@gmail.com>
2019-01-12 22:08:00 +0300

    fixed pyzmail library bug locally

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
 import imapclient
 import pyzmail
 
-def get_emails_body():
-    imapObj = imapclient.IMAPClient('imap.gmail.com', ssl = True)
-    imapObj.login('valery.voitova@gmail.com', 'hrbottest2206')
-    imapObj.select_folder('Candidates', readonly = False)
-    UIDs = imapObj.search(['UNANSWERED'])
-    rawMessages = imapObj.fetch(UIDs, ['BODY[]'])
+imapObj = imapclient.IMAPClient('imap.gmail.com', ssl = True)
+imapObj.login('valery.voitova@gmail.com', 'hrbottest2206')
+imapObj.select_folder('Candidates', readonly = True)
+UIDs = imapObj.search(['UNANSWERED'])
+rawMessages = imapObj.fetch(UIDs, ['BODY[]'])
+print(rawMessages)
 
-    message = pyzmail.PyzMessage.factory(rawMessages['BODY'])
+for key, rawMessage in rawMessages.items():
+    message = pyzmail.PyzMessage.factory(rawMessage['BODY[]'])
     message_body = message.text_part.get_payload().decode(message.text_part.charset)
     print(message_body)
-
-get_emails_body()
